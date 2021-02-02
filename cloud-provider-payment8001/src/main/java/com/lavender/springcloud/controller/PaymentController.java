8925b33a6f21b107b47a9a9c9f4de7349db3cca8
lavenderrem <1016705357@qq.com>
2021-02-02 22:25:15 +0800

    添加服务发现

--- cloud-provider-payment8001/src/main/java/com/lavender/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/lavender/springcloud/controller/PaymentController.java
@@ -4,14 +4,18 @@ package com.lavender.springcloud.controller;
 import com.lavender.springcloud.entity.CommonResult;
 import com.lavender.springcloud.entity.Payment;
 import com.lavender.springcloud.service.PaymentService;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -20,6 +24,8 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
     @Value("${server.port}")
     private String serverPort;
 
@@ -43,6 +49,21 @@ public class PaymentController {
             return new CommonResult(444,"没有对应记录，查询ID："+id,null);
         }
     }
+
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("***** element:"+element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
+    }
+
 }
 
 
