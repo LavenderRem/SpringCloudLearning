f1f26f10e9c0e08ec71c7dcd9cb56935c37ea00a
lavenderrem <1016705357@qq.com>
2021-02-07 22:22:07 +0800

    OpenFeign服务接口调用

---
+++ cloud-consumer-feign-order80/src/main/java/com/lavender/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,23 @@
+package com.lavender.springcloud.controller;
+
+
+import com.lavender.springcloud.entity.CommonResult;
+import com.lavender.springcloud.entity.Payment;
+import com.lavender.springcloud.service.PaymentFeignService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class OrderFeignController {
+
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        return paymentFeignService.getPaymentById(id);
+    }
+}
